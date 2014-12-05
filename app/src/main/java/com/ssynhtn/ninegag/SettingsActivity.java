fd5cd07a938decffbe5afdb1d0fef1033ecbedba
ssynhtn <ssynhtn@gmail.com>
2014-12-05 13:08:49 +0800

    make preference summray show the current value

--- app/src/main/java/com/ssynhtn/ninegag/SettingsActivity.java
+++ app/src/main/java/com/ssynhtn/ninegag/SettingsActivity.java
@@ -1,8 +1,12 @@
 package com.ssynhtn.ninegag;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 
 public class SettingsActivity extends Activity {
@@ -19,7 +23,7 @@ public class SettingsActivity extends Activity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
         public SettingsFragment() {
         }
@@ -28,6 +32,33 @@ public class SettingsActivity extends Activity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.key_num_columns)));
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference){
+            preference.setOnPreferenceChangeListener(this);
+
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String value = prefs.getString(preference.getKey(), "");
+            onPreferenceChange(preference, value);
+        }
+
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+
+            if(preference instanceof ListPreference){
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+                CharSequence displayValue = listPreference.getEntries()[index];
+                listPreference.setSummary(displayValue);
+            } else {
+                preference.setSummary(stringValue);
+            }
+
+            return true;
         }
     }
 }
